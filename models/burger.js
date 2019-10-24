cf0ad3492240928852fabf8b4b62b5bb890dc6a2
ron erlih <ronerlih@gmail.com>
2019-10-23 20:52:53 -0400

    first commit

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+// Burger models
+
+// The burger has a burger_name attribute of type DataTypes.String
+// and a devoured attribute that is false by default
+
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  });
+
+    Burger.associate = function(models) {
+        Burger.hasOne(models.Customer);
+      }
+
+  return Burger;
+};
